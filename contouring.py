b26092f09c958c9165e2809c6a29b24cc05a8a19
juankroos <58156316+juankroos@users.noreply.github.com>
2025-09-17 14:19:43 +0100

    commit for edge

--- contouring.py
+++ contouring.py
@@ -61,25 +61,7 @@ def medianfilter(img, k):
     dst = dst[size:size+w,size:size+h].astype(np.uint8)
     return dst
 
-def edge_drawing(image):
-    # Convertir l'image en niveaux de gris si nécessaire
-    if image.ndim == 3:
-        image = np.mean(image, axis=2)
-    
-    # Appliquer un seuillage pour obtenir une image binaire
-    threshold = np.mean(image)
-    binary_image = (image > threshold).astype(np.uint8) * 255
-    
-    # Trouver les contours
-    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # Créer une image pour dessiner les contours
-    contour_image = np.zeros_like(image, dtype=np.uint8)
-    
-    # Dessiner les contours
-    cv2.drawContours(contour_image, contours, -1, (255), thickness=1)
-    
-    return contour_image
+
 
 # Exemple de points aléatoires
 points = np.array([
@@ -91,14 +73,14 @@ points = np.array([
 
 # Calcul du carré englobant
 x, y, s = carre_englobant(points)
-test = load_image_as_matrix('billes.png')
-contour_image = edge_drawing(test)
+test = plt.imread('billes.png')
+
 
 # Tracé
-a = plt.imshow(contournaif(test[:,:]), cmap='gray')
+a = plt.imshow(contournaif(test[:,:,0]))
 #plt.figure(figsize=(6, 6))
 #plt.scatter(points[:, 0], points[:, 1], color='blue', label='Points')
-plt.gca().add_patch(plt.Rectangle((x, y), s, s, edgecolor='red', facecolor='none', lw=2, label='Carré englobant'))
+#plt.gca().add_patch(plt.Rectangle((x, y), s, s, edgecolor='red', facecolor='none', lw=2, label='Carré englobant'))
 #plt.title("Carré englobant autour des points")
 #plt.xlabel("x")
 #plt.ylabel("y")
