a693a0de3f3c23af8a701b3a5d6d23f8d5670f3b
juankroos <58156316+juankroos@users.noreply.github.com>
2025-09-23 14:07:56 +0100

    update

--- cartography.py
+++ cartography.py
@@ -1,18 +1,19 @@
 import os
-import time
 
-def watch_new_file(file_path):
-    return os.listdir(file_path)
+WATCH_PATH = r"C:\Users\juankroos\PycharmProjects\AgentIA\Agent"
+known_files = set(os.listdir(WATCH_PATH))
 
 
-path = r"C:\Users\juankroos\PycharmProjects\AgentIA\Agent"
-dirr = os.getcwd()
-dir = watch_new_file(path)
-print(dir)
-for d in dir:
-    print(dirr + "\\" + d)
-    current_file1 = (dirr + "\\" + d)
-    print(current_file1)
-    while True:
-        pass
+def check_new_files() -> list[str]:
+    """
+    Retourne les nouveaux fichiers detectes dans le dossier WATCH_PATH.
+    """
+    global known_files
+    current_files = set(os.listdir(WATCH_PATH))
+    new_files = list(current_files - known_files)
+    known_files = current_files
+    path = r"C:\Users\juankroos\PycharmProjects\AgentIA\Script\script_instruction.txt"
+    return path
 
+a = check_new_files()
+print(a)
